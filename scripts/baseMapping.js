603f4822e2c64371d7e988580d481f92bff9b8b9
William <willie@elaba.net>
2023-12-21 17:26:41 +0100

    overhaul

--- scripts/baseMapping.js
+++ scripts/baseMapping.js
@@ -1,18 +1,28 @@
 class baseMapping {
-    fieldMappings = {};
-    logPrefix = "Export Sheet";
-    systemName = "test";
 
     constructor(actor, sheetType, sheet) {
+        console.group("baseMapping.constructor");
         this.sheetType = sheetType;
-        console.log("sheetType:", this.sheetType);
         this.actor = actor;
         this.sheet = sheet;
-        console.log("actor:", this.actor);
+        console.debug("sheetType:", this.sheetType);
+        console.debug("actor:", this.actor);
+        console.groupEnd()
+        /*
+        schema for fieldMappings:
+          pdf: string, pdf fieldname
+          content: string, what to use to eval
+          calculated: string, the result of eval(content)
+          font: string, the name of the font
+          font_size: int, the size of the associated font
+        */
+        this.fieldMappings = [];
+        this.logPrefix = "Export Sheet";
+        this.systemName = "test";
         this.createMappings();
     }
 
-    #log(severity, message, options) {
+    log(severity, message, options) {
         /* A wrapper around the foundry vtt logger or `console`, whichever is present */
         if ( typeof(ui) !== "undefined" ) {
             switch(severity) {
@@ -47,43 +57,75 @@ class baseMapping {
         }
     }
 
-    #logDebug(message, options) {
-        this.#log("debug", message, options);
+    logDebug(message, options) {
+        this.log("debug", message, options);
     }
 
-    #logError(message, options) {
-        this.#log("error", message, options);
+    logError(message, options) {
+        this.log("error", message, options);
     }
 
-    #logInfo(message, options) {
-        this.#log("info", message, options);
+    logInfo(message, options) {
+        this.log("info", message, options);
     }
 
-    #logWarning(message, options) {
-        this.#log("warning", message, options);
+    logWarning(message, options) {
+        this.log("warning", message, options);
     }
 
+    updateMapping(fieldName, data) {
+        fieldName = fieldName.trim();
+        let el = this.fieldMappings.filter(i => i.pdf === fieldName);
+        if (el.length === 0) {
+            el = Object.assign({"pdf": fieldName}, data);
+        } else if (el.length > 1) {
+            this.logError("More than one element was found with pdf field name ${fieldName}! What is going on?");
+        } else {
+            el = Object.assign(el[0], data);
+        }
+        this.fieldMappings = this.fieldMappings.filter(i => i.pdf !== fieldName);
+        this.fieldMappings.push(el);
+    }
+
+    /* duplicate method name
     getMapping(fieldName) {
-        /* pdf field getter */
-        if (Object.keys(this.fieldMappings).includes(fieldName)) {
+        // pdf field getter
+        if (this.fieldMappings.map(i => i.pdf).includes(fieldName)) {
             return this.fieldMappings[fieldName];
         } else {
-            this.#logWarning(`${fieldName} does not exist`);
-            return undefined;
+            this.logWarning(`${fieldName} does not exist`);
+            return {};
         }
     }
+    */
 
-    setMapping(fieldName, value, force) {
-        /* pdf field setter */
-        if ( !Object.keys(this.fieldMappings).includes(fieldName) || force) {
-            this.fieldMappings[fieldName] = value;
-        } else {
-            this.#logWarning(`${fieldName} is already defined. Only using the first value`);
-        }
+    setCalculated(fieldName, value, force=false) {
+        fieldName = fieldName.trim();
+        this.updateMapping(fieldName, {"calculated": value});
     }
 
-    createMappings = function(){
+    setFont(fieldName, fontName=undefined, fontSize=undefined, force=false) {
+        fieldName = fieldName.trim();
+        let font = {};
+        if ( fontName !== undefined ) {
+            font.font = String(fontName).trim();
+        }
+        if (!isNaN(parseInt(fontSize))) {
+            font.font_size = parseInt(fontSize);
+        }
+        if ( Object.keys(font).length > 0 ) {
+            this.updateMapping(fieldName, font);
+            return true;
+        }
+        return false;
+    }
 
+    createMappings(){
+        this.fieldMappings = [];
+    }
+
+    get fields() {
+        return this.fieldMappings;
     }
 }
 
