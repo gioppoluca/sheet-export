76801505326c1f7007d379976080b4e0fddcdc36
Luca Gioppo <gioppoluca@libero.it>
2024-01-27 12:05:26 +0100

    feature: added npc for dnd5e

--- mappings/dnd5e/standard/latest/player.js
+++ mappings/dnd5e/standard/latest/player.js
@@ -424,48 +424,6 @@ class MappingClass extends baseMapping {
         this.setCalculated("Check Box 3081", this.actor.items.filter(i => i.type === 'spell' && i.system.level === 9)[4]?.system.preparation.prepared || "");
         this.setCalculated("Check Box 3082", this.actor.items.filter(i => i.type === 'spell' && i.system.level === 9)[5]?.system.preparation.prepared || "");
         this.setCalculated("Check Box 3083", this.actor.items.filter(i => i.type === 'spell' && i.system.level === 9)[6]?.system.preparation.prepared || "");
-        this.mapCompleteSpells();
-        this.mapEquipment();
-    }
-
-
-    mapCompleteSpells() {
-        let orderedSpells = this.actor.items.filter(i => i.type === 'spell').sort((a, b) => { return (a.system.level - b.system.level || a.name.localeCompare(b.name)) })
-        const maxSpells = orderedSpells.length < 80 ? orderedSpells.length : 80;
-        for (let index = 0; index < maxSpells; index++) {
-            const theSpell = orderedSpells[index];
-            const spellIndex = (index + 1).toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-            this.setCalculated(`spell_name_${spellIndex}`, theSpell.name + ((typeof theSpell.flags['items-with-spells-5e'] !== 'undefined') ? '[' + fromUuidSync(theSpell.flags['items-with-spells-5e']['parent-item']).name + ']' : '') || "");
-            this.setCalculated(`spell_school_${spellIndex}`, theSpell.system.school || "");
-            this.setCalculated(`spell_level_${spellIndex}`, theSpell.system.level || 0);
-            this.setCalculated(`spell_description_${spellIndex}`, (function (h) {
-                const d = document.createElement("div");
-                d.innerHTML = h;
-                return d.textContent || d.innerText || "";
-            })(theSpell.system.description.value || ""));
-            this.setCalculated(`spell_verbal_${spellIndex}`, theSpell.system.components.vocal || 0);
-            this.setCalculated(`spell_somatic_${spellIndex}`, theSpell.system.components.somatic || 0);
-            this.setCalculated(`spell_material_${spellIndex}`, theSpell.system.components.material || 0);
-            this.setCalculated(`spell_ritual_${spellIndex}`, theSpell.system.components.ritual || 0);
-            this.setCalculated(`spell_concentration_${spellIndex}`, theSpell.system.components.concentration || 0);
-            this.setCalculated(`spell_range_${spellIndex}`, theSpell.system.range.value + " " + theSpell.system.range.units || "");
-            this.setCalculated(`spell_casting_${spellIndex}`, theSpell.system.activation.cost + " " + theSpell.system.activation.type || "");
-            this.setCalculated(`spell_duration_${spellIndex}`, theSpell.system.duration.value + " " + theSpell.system.duration.units || "");
-        }
-    }
-
-    mapEquipment(){
-        this.setGlobalValue("equipment_extended",this.actor.items.filter(i => ['weapon', 'equipment', 'tool', 'consumable', 'loot','backpack'].includes(i.type)).map(i => `${i.name} (${i.system.quantity}): \n${((h) => {
-            const d = document.createElement("div");
-            d.innerHTML = h;
-            return d.textContent || d.innerText || "";
-        })(i.system.description.value)}\n`).join("\n"));
-
-        this.setCalculated("equipment_extended1", this.getGlobalValue("equipment_extended",0,2500));
-        this.setCalculated("equipment_extended2", this.getGlobalValue("equipment_extended",2500,5000));
-        this.setCalculated("equipment_extended3", this.getGlobalValue("equipment_extended",5000,7500));
-        this.setCalculated("equipment_extended4", this.getGlobalValue("equipment_extended",7500,10000));
-        
     }
 
     getPrimaryClassObj() {
